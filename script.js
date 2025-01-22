26a0af0065d67907297edcac697580abdc8e0e3e
neo <tlmarcoss@gmail.com>
2025-01-22 11:53:51 +0900

    all

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById("tigran").addEventListener("click", function() {
+    alert("Você clicou no botão Tigran");
+});
+
+document.getElementById("marcos").addEventListener("click", function() {
+    alert("Você clicou no botão Marcos");
+});
+
+document.getElementById("john").addEventListener("click", function() {
+    alert("Você clicou no botão John");
+});
